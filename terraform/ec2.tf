09218d993f6b53d3d46fb9c2650202f4667e1ae1
kennykenny <gbmkocc1@gmail.com>
2025-06-10 17:52:53 +0100

    new new

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "secure_bastion" {
   key_name                    = var.ec2_key_name  # ✅ REPLACE with your actual key name
     subnet_id                   = aws_subnet.public_subnet.id       # ✅ NEW
   vpc_security_group_ids      = [aws_security_group.ssh_access.id] # ✅ NEW
-  associate_public_ip_address = true
+  associate_public_ip_address = false
 
   tags = {
     Name = "SecureBastionEC2"
